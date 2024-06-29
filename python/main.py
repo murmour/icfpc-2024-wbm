044c740b85cf62c491a4c6f2ddebf2c4eb63a17f
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2024-06-29 17:26:44 +0500

    3d download

--- python/main.py
+++ python/main.py
@@ -4,33 +4,32 @@ import time
 def interact(request):
     return subprocess.check_output(['../../data/ocaml_bins/interact', '-ss', request])
 
-def download_lambdaman():
-    for i in range(1, 22):
-        input_file = '../../data/lambdaman/%02d.in' % i
+
+def download(request, path, n):
+    for i in range(1, n+1):
+        input_file = '%s/%02d.in' % (path, i)
         if os.path.isfile(input_file):
-            print('Skip %d' % i)
+            print('Skip %d' %i)
             continue
         print('%d...' % i)
-        request = 'get lambdaman%d' % i
-        data = interact(request)
+        data = interact(request % i)
         print(data)
         with io.open(input_file, 'wb') as f:
             f.write(data)
         time.sleep(4)
 
+
+def download_lambdaman():
+    download("get lambdaman%d", "../../data/lambdaman", 21)
+
+
 def download_spaceship():
-    for i in range(1, 26):
-        input_file = '../../data/spaceship/%02d.in' % i
-        if os.path.isfile(input_file):
-            print('Skip %d' % i)
-            continue
-        print('%d...' % i)
-        request = 'get spaceship%d' % i
-        data = interact(request)
-        print(data)
-        with io.open(input_file, 'wb') as f:
-            f.write(data)
-        time.sleep(4)
+    download("get spaceship%d", "../../data/spaceship", 25)
+
+
+def download_3d():
+    download("get 3d%d", "../../data/3d", 12)
+
 
 def create_folders(task_name, cnt):
     for i in range(cnt):
@@ -38,10 +37,14 @@ def create_folders(task_name, cnt):
         fname = '../../data/out/%s/%02d' % (task_name, n)
         os.makedirs(fname, exist_ok=True)
 
+
 def main():
     #download_lambdaman()
     #download_spaceship()
+    download_3d()
     #create_folders("spaceship", 25)
-    create_folders("lambdaman", 21)
+    #create_folders("lambdaman", 21)
 
-main()
+
+if __name__ == "__main__":
+    main()
