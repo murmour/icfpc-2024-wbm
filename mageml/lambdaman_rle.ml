555e024c8d8f2217d1e62614d4c9be3dfcacfd26
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2024-06-30 16:05:19 +0500

    lambdaman optimization

--- mageml/lambdaman_rle.ml
+++ mageml/lambdaman_rle.ml
@@ -1,31 +1,46 @@
 let loop a =
   (fun b -> a (b b))(fun b -> a (b b));
 
-let map_char (i: int): string =
-    take 1 (drop i "URDL");
+(*let map_char (i: int): string =
+    take 1 (drop i "URDL");*)
 
-
-let copy_str (s: string) (num: int) : string =
-    let copy_loop = loop(fun copy (s: string) (n: int) : string ->
-        if n = 1 then s
-        else (copy s (n - 1)) ^ s
-    ) in
-    copy_loop s num;
-
-let rle_decode (block: int) (count_p: int) (char_p: int) : string =
+(*let rle_decode (block: int) (count_p: int) (char_p: int) : string =
     let loop1 = loop(fun dec (b: int) (c_p: int) (ch_p: int) : string ->
-        let c = b % ch_p in
-        let b1 = b / ch_p in
-        let n = b1 % c_p in
-        let b2 = b1 / c_p in
-        let tmp = copy_str (map_char (trace c)) (trace n) in
-
-        if b2 > 0 then
-            (dec b2 c_p ch_p) ^ tmp
+        let copy_loop = loop(fun copy (s: string) (n: int) : string ->
+            if n = 1 then s
+            else (copy s (n - 1)) ^ s
+        ) in
+        if b > 0 then
+            (dec ((b / ch_p) / c_p) c_p ch_p) ^ (copy_loop (map_char (b % ch_p)) ((b / ch_p) % c_p))
         else
-            tmp
+            ""
     ) in
-    loop1 block count_p char_p;
+    loop1 block count_p char_p;*)
 
+(*let rle_decode (block: int) : string =
+    let loop1 = loop(fun dec (b: int) : string ->
+        let copy_loop = loop(fun copy (s: string) (n: int) : string ->
+            if n = 1 then s
+            else (copy s (n - 1)) ^ s
+        ) in
+        if b > 0 then
+            (dec (b / 16)) ^ (copy_loop (take 1 (drop (b % 4) "URDL")) ((b / 4) % 4))
+        else
+            ""
+    ) in
+    loop1 block;*)
 
-let magic = "solve lambdaman4 " ^ (rle_decode 52647 4 4);
+(* let magic = "solve lambdaman4 " ^ (rle_decode 52647); *)
+
+let magic =
+    let loop1 = loop(fun dec (b: int) : string ->
+        let copy_loop = loop(fun copy (s: string) (n: int) : string ->
+            if n = 1 then s
+            else (copy s (n - 1)) ^ s
+        ) in
+        if b > 0 then
+            (dec (b / 16)) ^ (copy_loop (take 1 (drop (b % 4) "URDL")) ((b / 4) % 4))
+        else
+            ""
+    ) in
+    "solve lambdaman4 " ^ (loop1 52647);
